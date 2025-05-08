a9bdc362a97d0f5fe7738cc4563bc21618257a19
bingoo30 <biwang01@ucm.es>
2025-05-08 02:18:53 +0200

    nuevo manipulator

--- Proyecto_final_dsi/Assets/Resources/scripts/DSIManipulator.cs
+++ Proyecto_final_dsi/Assets/Resources/scripts/DSIManipulator.cs
@@ -38,17 +38,35 @@ public class DSIManipulator : MouseManipulator
     }
     private void SelectElement(VisualElement element)
     {
-        element.style.borderLeftColor = Color.white;
-        element.style.borderRightColor = Color.white;
-        element.style.borderTopColor = Color.white;
-        element.style.borderBottomColor = Color.white;
+        //element.style.borderLeftColor = Color.white;
+        //element.style.borderRightColor = Color.white;
+        //element.style.borderTopColor = Color.white;
+        //element.style.borderBottomColor = Color.white;
+        Texture2D tex = Resources.Load<Texture2D>("sprites/selection");
+        Debug.Log(tex);
+        VisualElement overlay = new VisualElement();
+        overlay.style.position = Position.Absolute;
+        overlay.style.top = 0;
+        overlay.style.left = 0;
+        overlay.style.width = element.resolvedStyle.width;
+        overlay.style.height = element.resolvedStyle.height;
+        overlay.style.backgroundImage = new StyleBackground(tex);
+        overlay.name = "selectionOverlay";
+
+        element.Add(overlay);
+        Debug.Log(overlay.name);
     }
 
     private void ResetSelection(VisualElement element)
     {
-        element.style.borderLeftColor = Color.black;
-        element.style.borderRightColor = Color.black;
-        element.style.borderTopColor = Color.black;
-        element.style.borderBottomColor = Color.black;
+        //element.style.borderLeftColor = Color.black;
+        //element.style.borderRightColor = Color.black;
+        //element.style.borderTopColor = Color.black;
+        //element.style.borderBottomColor = Color.black;
+        var overlay = element.Q("selectionOverlay");
+        if (overlay != null)
+        {
+            element.Remove(overlay);
+        }
     }
 }
