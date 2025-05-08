c2fe2a8b1b7c291b146f7b8eba69780d18d4a74e
MaEduardaB <mbeckers@ucm.es>
2025-05-08 11:46:31 +0200

    jeugo en el flujo

--- Proyecto_final_dsi/Assets/Resources/scripts/SceneManager.cs
+++ Proyecto_final_dsi/Assets/Resources/scripts/SceneManager.cs
@@ -1,6 +1,7 @@
 using stars_namespace;
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 using UnityEngine.UIElements;
 
@@ -59,6 +60,37 @@ public class SceneManager : MonoBehaviour
     VisualElement _game_menu;
     VisualElement _pause_game_menu;
 
+    List<VisualElement> bichosContainers;
+    //indice para acceder a bichito concreto
+    int act_indice = 0;
+    Button antes;
+    Button despues;
+    private void esconderTodo() {
+        foreach (VisualElement v in bichosContainers) {
+            v.style.display = DisplayStyle.None;
+        }
+    }
+    private void actualizar(bool antes) {
+        int ind = 0;
+        if (antes) {
+            Debug.Log($"actual indice (antes): {act_indice}");
+            //no podemos tener indices negativos
+            ind = Mathf.Max(act_indice - 1, 0);
+        } else {
+            //no podemos tener superiores al tamaño de la lista
+            ind = Mathf.Min(act_indice + 1, bichosContainers.Count - 1);
+        }
+
+        //solo hacemos cosas cuando es valido
+        if (ind >= 0 && ind < bichosContainers.Count) {
+            //esconder todos los bichos
+            esconderTodo();
+            //actualizar el indicador y ponerlo como visible
+            act_indice = ind;
+            bichosContainers[act_indice].style.display = DisplayStyle.Flex;
+        }
+    }
+    
     //menu de niveles
     private Label _estrellaTotal;
     private List<NivelData> _niveles;
@@ -285,6 +317,32 @@ public class SceneManager : MonoBehaviour
         InitializeLevelMenu();
 
         ShowMenu(_initial_menu);
+
+
+
+        List<VisualElement> bichos = root.Q("bichos").Children().ToList();
+        bichos.ForEach(b => {
+            List<VisualElement> x = b.Children().ToList();
+            x.ForEach(e => {
+                e.pickingMode = PickingMode.Position;
+                e.AddManipulator(new DSIManipulator());
+                e.AddManipulator(new AnimalDragHandler());
+            });
+        });
+
+        //referenciar la lista de elementos
+        bichosContainers = root.Q<VisualElement>("bichos").Children().ToList();
+        antes = root.Q<Button>("ante");
+        despues = root.Q<Button>("despues");
+
+        despues.RegisterCallback<ClickEvent>(e => {
+            Debug.Log("click despues");
+            actualizar(false);
+        });
+        antes.RegisterCallback<ClickEvent>(e => {
+            Debug.Log("click antes");
+            actualizar(true);
+        });
     }
 
     private void ShowMenu(VisualElement menu) {
